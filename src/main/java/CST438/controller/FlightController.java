78e64b8d001a428dfacccb2e589576a1e22702b8
Chaoslime <nicholas3000@sbcglobal.net>
2020-07-22 18:23:52 -0700

    Added POST mapping for form to be passed into the API once created.

--- src/main/java/CST438/controller/FlightController.java
+++ src/main/java/CST438/controller/FlightController.java
@@ -1,37 +1,55 @@
 package CST438.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import CST438.domain.FlightInfo;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import CST438.domain.LocationInfo;
+import CST438.domain.ReservationRepository;
 
 @Controller
 public class FlightController {
 
-  // @Autowired
-  // MovieRatingRepository movieRatingRepository;
 
-  @GetMapping("/reservation/new")
-  public String createRating(Model model) {
-    FlightInfo flight = new FlightInfo();
-    model.addAttribute("flight", flight);
+  // repo that holds flight reservations
+  @Autowired
+  ReservationRepository reservationRepository;
+
+  @GetMapping("/reservation")
+  public String getLocationInfo(Model model) {
+    LocationInfo location = new LocationInfo();
+    model.addAttribute("flight", location);
     return "reservation_form";
   }
 
-  /*
-   * @PostMapping("/movies") public String processRatingForm(@Valid Rating rating, BindingResult
-   * result, Model model) { if(result.hasErrors()){ return "rating_form"; }
-   * movieRatingRepository.save(rating); newestID = rating.getId(); return ("redirect:/movies"); }
-   */
-  @GetMapping("/flights")
-  public String getAllRatings(Model model) {
-    // displays all ratings by title and chronological order desc
-    // TODO: pass object from database to be displayed into display_flight_listing
-    // Iterable<FlightInfo> flight = ReservationRepository.findAll();
-    // model.addAttribute("ratings", rating);
-    // inputs value for the most recent new rating and returns it when added.
-    // model.addAttribute("mostRecentRating", newestID);
+  @PostMapping("/flights")
+  public String getAllFlights(@RequestParam("originCity") String originCity,
+      @RequestParam("destinationCity") String desinationCity,
+      @RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate,
+      Model model) {
+
+    model.addAttribute("origin", originCity);
+    model.addAttribute("destination", desinationCity);
+    model.addAttribute("startDate", startDate);
+    model.addAttribute("endDate", endDate);
+
+    // TODO: set up and retrieve API for geolocation for reservation/api page
+    // TODO: return geolocation object from API and pass into flight API.
+    // TODO: set up and retrive API for flight info.
+    // TODO: use flight API object for both locations, and return object to be passed
+    // into the display_flight_listing page
 
     return "display_flight_listing";
+
+  }
+
+  @GetMapping("/receipt")
+  public String showRecept(Model model) {
+
+    // TODO: pass API object created from the booking service.
+    return "reservation_receipt";
+
   }
 }
