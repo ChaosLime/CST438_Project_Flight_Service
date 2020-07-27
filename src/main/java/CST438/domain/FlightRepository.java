b8eff55a66372613aa7b5a6d56d8e27377f34999
mikie Reed <mikie@prenotions.com>
2020-07-27 11:03:18 -0700

    Update FlightRepository query

--- src/main/java/CST438/domain/FlightRepository.java
+++ src/main/java/CST438/domain/FlightRepository.java
@@ -3,9 +3,16 @@ package CST438.domain;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FlightRepository extends JpaRepository<Flight, Long> {
-  List<Flight> findByName(String name);
+
+  @Query("SELECT flight FROM dummy_flight_data WHERE flight.DATE = :date AND flight.DEPARTUREAIRPORT = :departureAirport AND flight.ARRIVALAIRPORT = :arrivalAirport")
+  List<Flight> findByDateAndAirports(@Param("date") String date,
+      @Param("departureAirport") String departureAirport,
+      @Param("arrivalAirport") String arrivalAirport);
+
 }
