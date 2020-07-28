68e4aa2af48a8eb4113e47a63ca8a25626fc4e4a
Chaoslime <nicholas3000@sbcglobal.net>
2020-07-27 22:20:24 -0700

    Updated controller to handle validations for form.

--- src/main/java/CST438/controller/FlightController.java
+++ src/main/java/CST438/controller/FlightController.java
@@ -1,17 +1,16 @@
 package CST438.controller;
 
 import java.util.List;
-
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import CST438.domain.Flight;
-import CST438.domain.LocationInfo;
-import CST438.domain.ReservationRepository;
+import CST438.domain.FormInfo;
 import CST438.service.FlightService;
 
 @Controller
@@ -29,30 +28,55 @@ public class FlightController {
   }
 
   @GetMapping("/reservation")
-  public String getLocationInfo(Model model) {
-    LocationInfo location = new LocationInfo();
-    model.addAttribute("flight", location);
+  public String getFormInfo(Model model) {
+    FormInfo formInfo = new FormInfo();
+    model.addAttribute("formInfo", formInfo);
     return "reservation_form";
   }
 
   @PostMapping("/flights")
-  public String getAllFlights(@RequestParam("originCity") String originCity,
+  public String getAllFlights(@Valid FormInfo formInfo, BindingResult result,
+      @RequestParam("originCity") String originCity,
       @RequestParam("destinationCity") String destinationCity,
       @RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate,
       Model model) {
 
-    startDate = formatFlightDate(startDate);
-    List<Flight> departureFlights = flightService.getFlightList(startDate, originCity,
-        destinationCity);
+    if (result.hasErrors()) {
+      return "reservation_form";
+    }
 
+    String flightNotFound;
+
+    startDate = formatFlightDate(startDate);
+    List<Flight> departureFlights =
+        flightService.getFlightList(startDate, originCity, destinationCity);
+
+    // If no departure flights are found
+    if (departureFlights.isEmpty()) {
+      flightNotFound = "No departure flights found, please try a different date or destination";
+    } else {
+      flightNotFound = "";
+    }
+
+    model.addAttribute("departNotFound", flightNotFound);
     model.addAttribute("departDate", startDate);
     model.addAttribute("departureFlights", departureFlights);
+
 
     endDate = formatFlightDate(endDate);
     List<Flight> returnFlights = flightService.getFlightList(endDate, destinationCity, originCity);
 
+    // If no return flights are found
+    if (returnFlights.isEmpty()) {
+      flightNotFound = "No return flights found, please try a different date or destination";
+    } else {
+      flightNotFound = "";
+    }
+
+    model.addAttribute("returnNotFound", flightNotFound);
     model.addAttribute("returnDate", endDate);
     model.addAttribute("returnFlights", returnFlights);
+
 
     return "display_flight_listing";
   }
