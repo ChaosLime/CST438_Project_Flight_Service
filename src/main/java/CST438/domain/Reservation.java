f09b6f0eb03da6531f3c89cf0bfe955b638c5eae
Chaoslime <nicholas3000@sbcglobal.net>
2020-08-04 00:03:17 -0700

    Made reservation Entity that handles users and seats

--- src/main/java/CST438/domain/Reservation.java
+++ src/main/java/CST438/domain/Reservation.java
@@ -1,53 +1,63 @@
 package CST438.domain;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-
-import javax.persistence.OneToOne;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="reservations")
-public class Reservation extends AbstractEntity{
+@Table(name = "reservations")
+public class Reservation {
 
-    private int comfId;
-    
-    @OneToOne
-    private User passenger;
-    
-    @OneToOne
-    private Flight flight;
+  @Id
+  @GeneratedValue
+  private long bookId;
+  private String userEmail;
+  private int departureFlightSeatInfoId;
+  private int returnFlightSeatInfoId;
+  // TODO Bool Cancelled?
 
-    public int getComfId() {
-        return comfId;
-    }
+  public Reservation() {
 
-    public void setComfId(int comfId) {
-        this.comfId = comfId;
-    }
+  }
 
-    public User getPassenger() {
-        return passenger;
-    }
+  public Reservation(String userEmail, int departureFlightSeatInfoId, int returnFlightSeatInfoId) {
+    super();
+    this.userEmail = userEmail;
+    this.departureFlightSeatInfoId = departureFlightSeatInfoId;
+    this.returnFlightSeatInfoId = returnFlightSeatInfoId;
+  }
 
-    public void setPassenger(User passenger) {
-        this.passenger = passenger;
-    }
+  public long getBookId() {
+    return bookId;
+  }
 
-    public Flight getFlight() {
-        return flight;
-    }
+  public void setBookId(long bookId) {
+    this.bookId = bookId;
+  }
 
-    public void setFlight(Flight flight) {
-        this.flight = flight;
-    }
+  public String getUserEmail() {
+    return userEmail;
+  }
 
-    @Override
-    public String toString() {
-        return "Reservation{" +
-                "comfId=" + comfId +
-                ", passenger=" + passenger +
-                ", flight=" + flight +
-                '}';
-    }
+  public void setUserEmail(String userEmail) {
+    this.userEmail = userEmail;
+  }
+
+  public int getDepartureFlightSeatInfoId() {
+    return departureFlightSeatInfoId;
+  }
+
+  public void setDepartureFlightSeatInfoId(int departureFlightSeatInfoId) {
+    this.departureFlightSeatInfoId = departureFlightSeatInfoId;
+  }
+
+  public int getReturnFlightSeatInfoId() {
+    return returnFlightSeatInfoId;
+  }
+
+  public void setReturnFlightSeatInfoId(int returnFlightSeatInfoId) {
+    this.returnFlightSeatInfoId = returnFlightSeatInfoId;
+  }
+
 }
