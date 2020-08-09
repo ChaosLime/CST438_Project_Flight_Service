f0899c21c18af51bba96877fc86c87f7b6a9941f
Chaoslime <nicholas3000@sbcglobal.net>
2020-08-09 16:04:06 -0700

    Removed unneeded comments, and whitespace

--- src/main/java/CST438/repository/FlightSeatInfoRepository.java
+++ src/main/java/CST438/repository/FlightSeatInfoRepository.java
@@ -1,24 +1,13 @@
 package CST438.repository;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import CST438.domain.FlightSeatInfo;
 
 @Repository
 public interface FlightSeatInfoRepository extends JpaRepository<FlightSeatInfo, String> {
-
-  // @Query(value = "SELECT * FROM dummy_flight_seat_information WHERE
-  // flight_number = ?1", nativeQuery = true)
   List<FlightSeatInfo> findByFlightNumber(int flightNumber);
 
   FlightSeatInfo findById(int id);
-  
- 
-  
-  
-  
 }
