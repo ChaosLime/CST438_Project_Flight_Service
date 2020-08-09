51c9252b32dcee3c6e061de8a142a67eddbfdc11
Chaoslime <nicholas3000@sbcglobal.net>
2020-08-09 16:07:28 -0700

    Updated formatting and whitespace.

--- src/main/java/CST438/service/ReservationService.java
+++ src/main/java/CST438/service/ReservationService.java
@@ -1,15 +1,10 @@
 package CST438.service;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import CST438.domain.FlightInfo;
 import CST438.domain.FlightSeatInfo;
 import CST438.domain.Reservation;
-import CST438.domain.User;
-import CST438.repository.FlightRepository;
 import CST438.repository.FlightSeatInfoRepository;
 import CST438.repository.ReservationRepository;
 
@@ -18,65 +13,54 @@ public class ReservationService {
 
   @Autowired
   private ReservationRepository reservationRepository;
-  
+
   @Autowired
   private FlightSeatInfoRepository flightSeatRepo;
 
   public void bookFlight(Reservation reservation) {
     reservationRepository.save(reservation);
   }
-  
 
   public void cancelFlight(Reservation reservation) {
-	  reservationRepository.save(reservation);
+    reservationRepository.save(reservation);
   }
-  
+
   public Reservation getBooking(long id) {
-	    
-	  Reservation booking = reservationRepository.findBookingByID(id);
-	  return booking;
-	      
+
+    Reservation booking = reservationRepository.findBookingByID(id);
+    return booking;
   }
-  
+
   public List<Reservation> getBookingList(String email) {
-	  
-	  List<Reservation> bookings = reservationRepository.findActiveBookings(email);
-	  return bookings;
-	    
+
+    List<Reservation> bookings = reservationRepository.findActiveBookings(email);
+    return bookings;
   }
-  
+
   public List<Reservation> getCxBookingList(String email) {
-	  
-	  List<Reservation> cx_bookings = reservationRepository.findCxBookings(email);
-	  return cx_bookings;
-	    
+
+    List<Reservation> cx_bookings = reservationRepository.findCxBookings(email);
+    return cx_bookings;
   }
-  
+
   public boolean cancelBooking(long id) {
-	    
-	  reservationRepository.cancelBooking(id);
-	    
-	  Reservation booking = reservationRepository.findBookingByID(id);
-	    
-	  if (booking.getIsCancelled()) {
-		  return true;
-	  } else {
-		  return false;
-	  }
-	  
+
+    reservationRepository.cancelBooking(id);
+
+    Reservation booking = reservationRepository.findBookingByID(id);
+
+    if (booking.getIsCancelled()) {
+      return true;
+    } else {
+      return false;
+    }
   }
-  
+
   public void updateSeat(int seatInfoId) {
 
-	    FlightSeatInfo seatInfo = flightSeatRepo.findById(seatInfoId);
+    FlightSeatInfo seatInfo = flightSeatRepo.findById(seatInfoId);
 
-	  
-	    seatInfo.setSeatsAvailable(seatInfo.getSeatsAvailable() + 1);
-	    flightSeatRepo.save(seatInfo);
-
-
+    seatInfo.setSeatsAvailable(seatInfo.getSeatsAvailable() + 1);
+    flightSeatRepo.save(seatInfo);
   }
-  
-    
-
 }
